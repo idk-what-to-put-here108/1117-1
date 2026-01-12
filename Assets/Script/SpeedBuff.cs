f41db94ca3d18ac82abe6b2ce09e98c90c0d7b76
Jack C. <143649009+idk-what-to-put-here108@users.noreply.github.com>
2026-01-12 16:04:35 -0500

    WIP speed buff 2

--- Assets/Script/SpeedBuff.cs
+++ Assets/Script/SpeedBuff.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class SpeedBuff : PowerupEffect
 {
+    public float amount;
     public override void Apply(GameObject target)
     {
-        
+        target.GetComponent<PlayerController>().speed
     }
 }
